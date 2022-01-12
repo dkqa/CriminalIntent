69db8dd31e9a055f83cdb2b793fa9ecbc317edc9
dkqa <dkqmoto@gmail.com>
2022-01-12 22:03:16 +0300

    Chapter 9 - List output and RecyclerView

---
+++ app/src/main/java/com/dkqa/android/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,17 @@
+package com.dkqa.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0..100) {
+            val crime = Crime()
+            crime.title = "Crime #$i"
+            crime.isSolved = i % 2 == 0
+            crimes += crime
+        }
+    }
+}
