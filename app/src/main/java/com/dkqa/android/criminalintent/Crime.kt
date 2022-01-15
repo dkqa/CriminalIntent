dc941a5b38c078f8126b6e03885f8d3755187420
dkqa <dkqmoto@gmail.com>
2022-01-15 18:47:57 +0300

    Chapter 11 - Database and Room Library

--- app/src/main/java/com/dkqa/android/criminalintent/Crime.kt
+++ app/src/main/java/com/dkqa/android/criminalintent/Crime.kt
@@ -1,8 +1,11 @@
 package com.dkqa.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-class Crime(val id: UUID = UUID.randomUUID(),
+@Entity
+class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
             var title: String = "",
             var date: Date = Date(),
             var isSolved: Boolean = false) {
