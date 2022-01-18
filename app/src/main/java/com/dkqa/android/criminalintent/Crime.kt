5171aa168a694bcf141cc79067f017bb145ad1b6
dkqa <dkqmoto@gmail.com>
2022-01-18 22:57:43 +0300

    Chapter 15 - Implicit intent

--- app/src/main/java/com/dkqa/android/criminalintent/Crime.kt
+++ app/src/main/java/com/dkqa/android/criminalintent/Crime.kt
@@ -5,9 +5,10 @@ import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity
-data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
-            var title: String = "",
-            var date: Date = Date(),
-            var isSolved: Boolean = false) {
-
-}
+data class Crime(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false,
+    var suspect: String = ""
+)
